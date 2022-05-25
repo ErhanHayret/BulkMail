71bdc9855eaef12ad138cd2b56a6c26bc693ee17
Erhan <erhan.durgun@outlook.com>
2022-05-25 18:40:28 +0300

    [chore]

--- src/GOProjects/Data/Models/Mail.go
+++ src/GOProjects/Data/Models/Mail.go
@@ -6,9 +6,9 @@ import(
 
 type Mail struct {
 	Id				primitive.ObjectID	`json:"id,omitempty"	bson:"_id"`
-	MailSubject    	string 				`json:"mailSubject" 	bson:"mail_subject"`
-	MailText 	   	string 				`json:"mailText" 		bson:"mail_test"`
-	SenderEmail    	string 				`json:"senderEmail" 	bson:"sender_email"`
-	SenderEmailPsw 	string				`json:"senderEmailPsw"	bson:"sender_email_psw"`
-	ArriveEmails   	[]string			`json:"arriveEmails" 	bson:"arrive_emails"`
+	MailSubject		string				`json:"mailSubject" 	bson:"mail_subject"`
+	MailText		string 				`json:"mailText"		bson:"mail_test"`
+	SenderEmail		string				`json:"senderEmail" 	bson:"sender_email"`
+	SenderEmailPsw	string				`json:"senderEmailPsw"	bson:"sender_email_psw"`
+	ArriveEmails	[]string			`json:"arriveEmails" 	bson:"arrive_emails"`
 }
